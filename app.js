7d3b71d44143a895f33e4772e6427b7a93cb28f7
unimexes2 <unimexes2@gmail.com>
2022-06-21 21:19:11 +0200

    2 iteration

--- app.js
+++ app.js
@@ -1,57 +1,27 @@
 const express = require("express");
 const app = express();
-const port = 8008;
+const port = 8080;
 app.use(express.static("public")); 
 
 app.get("/", (req, res) => {
-  res.status(200).send("Hello visitor!<br>nice to meet you"  
-  );
+  console.log("blin");
+  res.sendFile("/views/home.html");
 });
 
-app.get("/home", (req, res) => {
-  res.send("Pagina de Home");
+app.get("/photo", (req, res) => {
+  console.log("blin");
+
+  res.sendFile(__dirname + "/views/photo.html");
 });
 
-app.get("/casa", (req, res) => {
-  res.send("Esta es mi casa");
+app.get("/works", (req, res) => {
+  
+  res.sendFile(__dirname + "./views/works.html");
 });
-
-app.get("/friends/:friendParam", (req, res) => {
-  console.log(req.params);
-  //res.send("My friends")
-  if (req.params.friendParam === "Hola") {
-    res.send("esto lo he escrito");
-  } else if (req.params.friendParam === "Curro") {
-    res.send("Información sobre Curro");
-  } else {
-    res.send("No tengo mas amigos");
-  }
-});
-
-app.get("/speak/:languageParam", (req, res) => {
-  if (req.params.languageParam === "pig") {
-    res.send("The pig says 'Oink!'");
-  } else if (req.params.languageParam === "cow") {
-    res.send("The cow says 'Moo'");
-  } else if (req.params.languageParam === "dog") {
-    res.send("The dog says 'Woof Woof!'");
-  } else {
-    res.send("I dont have that lenguage'");
-  }
-});
-
-//greet/hola/3
-
-app.get("/greet/:greetWord/:timesGreet", (req, res) => {
-  const { greetWord, timesGreet } = req.params;
-  let fullMessage = greetWord.repeat(timesGreet);
-  res.send(fullMessage);
-});
-
-app.get("/mi-pagina", (req, res) => {
-  res.sendFile(__dirname + "/views/mi-pagina.html"); //ruta absoluta del elemento
+app.get("/about", (req, res) => {
+  res.sendFile(__dirname + "./views/about.html"); 
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`App listening on port ${port}`)
 });
